76206d29e01fe6bcea115a5fdd2d38f8a3eb4985
Reyner <reyenr1012002@gmail.com>
2024-09-03 04:03:12 -0600

    redireccion extra

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Development mode
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'http',
+    //             hostname: 'localhost',
+    //             port: '8000',
+    //             pathname: '/media/**',
+    //         },
+    //     ],
+    // },
+    // Production mode
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '8000',
+                protocol: 'https',
+                hostname: 'vbappback-74cfafa1439d.herokuapp.com',
                 pathname: '/media/**',
             },
         ],
