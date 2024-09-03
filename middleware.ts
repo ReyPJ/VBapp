38dec6cd7ccd9177312d484e5fd90a7547e8348a
Reyner <reyenr1012002@gmail.com>
2024-09-03 03:44:23 -0600

    redireccion extra

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,6 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const token = request.cookies.get('accessToken');
 
-    console.log('Token:', token);  // Añade esta línea para depurar
-
     if(!token) {
         return NextResponse.redirect(new URL ('/login', request.url));
     }
